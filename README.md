af64fed2bec6d7a34ca8855b021dc4aa807b7a45
Tino Fuhrmann <tino-fuhrmann@web.de>
2016-06-03 14:47:55 +0200

    Added script & Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+This script adds a toggle functionality to the discord channel list. Use Ctrl + H to toggle.
+
+# Install
+Add this script as a greasemonkey user script.
---
+++ hide_discord_channel_list.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        HideDiscordChannelSelect
+// @namespace   tifu
+// @include     https://discordapp.com/channels/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+function toggleServerList() { 
+  $('.flex-vertical.channels-wrap').animate({width: 'toggle'});; 
+}
+
+function loadJQuery(document) {
+      var script_jQuery = document.createElement('script'); 
+     script_jQuery.setAttribute('src', '//code.jquery.com/jquery-latest.min.js'); 
+     document.body.appendChild(script_jQuery);
+}
+
+console.log("test")
+window.onload = function() {
+  loadJQuery(document);
+}
+
+window.onkeydown = function (e) {
+    var code = e.keyCode ? e.keyCode : e.which;
+
+    if (e.ctrlKey && code === 72) { // ctrl + h
+       toggleServerList();
+    }
+};
